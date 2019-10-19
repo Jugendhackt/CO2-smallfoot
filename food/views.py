0f6552fb7fcca86842ea603f8632b5b823d88236
least-py <lestu9@icloud.com>
2019-10-19 19:12:02 +0200

    food_list erweitert und key value in der HTML-Datei hinzugefügt: Werte der Datenbank werden auf der Seite angezeigt

--- food/views.py
+++ food/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import food
 
 # Create your views here.
 
 def food_list(request):
-    return render(request, 'food/food_list.html', {})
+    carbon_val= food.objects.all()
+    #eingetragene Werte aufsummieren
+    #carbon_val = food.objects.filter(name=request)[0].CO2
+    return render(request, 'food/food_list.html', {'co2': carbon_val})
